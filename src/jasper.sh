5e9f41863571eaf0c59cf3fe5517622ea3c14e68
alguoo314 <76114178+alguoo314@users.noreply.github.com>
2024-06-23 19:38:07 -0400

    Update jasper.sh with indentation fix

--- src/jasper.sh
+++ src/jasper.sh
@@ -173,11 +173,11 @@ if [ -z ${JF_DB+x} ];then
           rm -f jasper.no_cat.success
         else
             log "Creating jellyfish database $JF_DB"
-	    JF_SIZE=$(echo "$JF_SIZE" | awk '{ printf "%.0f", $1 }')
-          zcat -f $READS | jellyfish count -C -s $JF_SIZE -m $KMER -o /dev/stdout -t $NUM_THREADS /dev/stdin | tee $JF_DB | jellyfish histo -t $NUM_THREADS /dev/stdin > jfhisto$KMER.csv.tmp && \
-          mv jfhisto$KMER.csv.tmp jfhisto$KMER.csv && \
-          touch jasper.no_cat.success  && \
-          touch jasper.histo.success
+	JF_SIZE=$(echo "$JF_SIZE" | awk '{ printf "%.0f", $1 }')
+        zcat -f $READS | jellyfish count -C -s $JF_SIZE -m $KMER -o /dev/stdout -t $NUM_THREADS /dev/stdin | tee $JF_DB | jellyfish histo -t $NUM_THREADS /dev/stdin > jfhisto$KMER.csv.tmp && \
+        mv jfhisto$KMER.csv.tmp jfhisto$KMER.csv && \
+        touch jasper.no_cat.success  && \
+        touch jasper.histo.success
         fi
     else
         error_exit "Either a jf database or files of polishing reads must be provided in the argument."
